65f3cc7b50bec350b1658756c05d1705c693988d
Abhijit180 <131157722+Abhijit180@users.noreply.github.com>
2026-02-06 11:58:01 +0530

    Update AdminController.java
    CORS policy updated

--- anonymous-confession/anonymous-confession/src/main/java/com/confess/anonymous/controller/AdminController.java
+++ anonymous-confession/anonymous-confession/src/main/java/com/confess/anonymous/controller/AdminController.java
@@ -5,13 +5,11 @@ import com.confess.anonymous.enums.Status;
 import com.confess.anonymous.repository.ConfessionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/admin/confessions")
 public class AdminController {
